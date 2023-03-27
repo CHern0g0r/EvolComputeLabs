fc82868f00ef1e9ee7921212adf79f223b6b8b66
CHern0g0r <fyodorchernogor@gmail.com>
2023-03-27 19:47:25 +0300

    Old lab

--- lab06/src/main/java/lab06/MyCrossover.java
+++ lab06/src/main/java/lab06/MyCrossover.java
@@ -11,9 +11,29 @@ public class MyCrossover extends AbstractCrossover<double[]> {
         super(1);
     }
 
-    protected List<double[]> mate(double[] p1, double[] p2, int i, Random random) {
-        ArrayList children = new ArrayList();
+    protected List<double[]> mate(double[] p1, double[] p2, int ncp, Random random) {
+        ArrayList<double[]> children = new ArrayList<double[]>();
 
+        // your implementation:
+
+        double[] c1 = p1.clone();
+        double[] c2 = p2.clone();
+        double x1, x2, val, l, a = 3.4;
+        l = random.nextDouble();
+
+        for (int j = 0; j < c1.length; j++) {
+            x1 = Math.min(p2[j], p1[j]);
+            x2 = Math.max(p2[j], p1[j]);
+            val = 1 - Math.pow(0.5, (x2 - x1) / 10.0);
+            c1[j] = val * (x1 + x2) + Math.pow(a, l) * Math.exp(1 - l) * (1 - val) * (x2 - x1);
+            c2[j] = (1 - val) * (x1 + x2) - Math.pow(a, (1 - l)) * Math.exp(l - 1.5 * l) * val * (x2 - x1);
+            
+            c1[j] = Math.min(Math.max(c1[j], -5), 5);
+            c2[j] = Math.min(Math.max(c2[j], -5), 5);
+        }
+
+        children.add(c1);
+        children.add(c2);
         return children;
     }
 }
--- lab06/src/main/java/lab06/MyMutation.java
+++ lab06/src/main/java/lab06/MyMutation.java
@@ -7,6 +7,28 @@ import java.util.Random;
 
 public class MyMutation implements EvolutionaryOperator<double[]> {
     public List<double[]> apply(List<double[]> population, Random random) {
+        for (int j = 0; j < population.size(); j++) {
+            double[] item = population.get(j);
+            double[] newitem = single(item, random);
+            population.set(j, newitem);
+        }
         return population;
+    }
+
+    private double[] single(double[] item, Random random) {
+        double c = 1;
+
+        int cnt = item.length / 2;
+        cnt = 1;
+        int idx;
+
+        for (int j = 0; j < cnt; j++) {
+            idx = random.nextInt(item.length);
+            c = random.nextDouble();
+            item[idx] += random.nextGaussian() * c;
+            item[idx] = Math.min(Math.max(item[idx], -5), 5);
+        }
+
+        return item;
     }
 }
